15b74b8e4b608a699c6b06d4e8f56ecf919ee99d
munteanusasha <alexi99.pl@gmail.com>
2019-04-21 01:42:42 +0300

    added another for style to ex. 3, (chess board)

--- Chapter2Exercises.js
+++ Chapter2Exercises.js
@@ -154,6 +154,21 @@
 	console.log(c);
 }
 
+//================		another for style		===============
+{
+	var board = "\n";
+	var size = 8;
+	for(var i = 0; i < size; i++){
+	    if(i % 2 == 0) board += " ";
+	    for(var j = 0; j < size; j++){
+	        if(j % 2 == 0) board += " ";
+	        else board += "#";
+	    }
+	    board += "\n";
+	}
+	console.log(board);
+}
+
 
 //=============================================================
 //==================	Author style	=======================
